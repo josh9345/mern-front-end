ebcb0afbb85c0116533499fdb055b5161b0a469e
Joshua Reed <joshualreed93@gmail.com>
2020-03-06 10:05:55 -0500

    search bars added

--- mern-front-end/src/App.js
+++ mern-front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Search from './search'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
          test
         </p>
       </header>
+      <Search />
     </div>
   );
 }
--- mern-front-end/src/search.js
+++ mern-front-end/src/search.js
@@ -5,7 +5,15 @@ const search = () => {
     return (
         <div>
             <form className='sideBar'>
+                <h3>Search by ingredients</h3>
                 <input type='input'></input>
+                <input type='submit'></input>
+                <h3>Search by name</h3>
+                <input type='input'></input>
+                <input type='submit'></input>
+                <h3>Search by dishtype</h3>
+                <input type='input'></input>
+                <input type='submit'></input>
             </form>
             
         </div>
