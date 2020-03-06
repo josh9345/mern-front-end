0c6b2cf17ec1413234374779a421209440992a9f
Joshua Reed <joshualreed93@gmail.com>
2020-03-06 09:48:15 -0500

    removed generic stuff add search

---
+++ mern-front-end/src/search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const search = () => {
+    return (
+        <div>
+            <form className='sideBar'>
+                <input type='input'></input>
+            </form>
+            
+        </div>
+    );
+};
+
+export default search;
